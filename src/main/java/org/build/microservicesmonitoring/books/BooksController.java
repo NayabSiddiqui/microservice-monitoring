428fcba9ba5ea9e8080d81014f3a80d9845bede8
Nayab Siddiqui <nayab.s86@gmail.com>
2018-07-03 21:22:15 +0530

    exposing actuator metrics via http

--- src/main/java/org/build/microservicesmonitoring/books/BooksController.java
+++ src/main/java/org/build/microservicesmonitoring/books/BooksController.java
@@ -3,11 +3,13 @@ package org.build.microservicesmonitoring.books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
+@RequestMapping(path = "books")
 public class BooksController {
 
     private final BooksService service;
