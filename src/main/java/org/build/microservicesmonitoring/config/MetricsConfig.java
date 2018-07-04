f069bfc5f7f3d6d20ba32cd3735c51a37786d90b
Nayab Siddiqui <nayab.s86@gmail.com>
2018-07-04 19:43:38 +0530

    exports metrics to influxdb

---
+++ src/main/java/org/build/microservicesmonitoring/config/MetricsConfig.java
@@ -0,0 +1,48 @@
+package org.build.microservicesmonitoring.config;
+
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class MetricsConfig {
+
+    private static final Duration HISTOGRAM_EXPIRY = Duration.ofMinutes(10);
+
+    private static final Duration STEP = Duration.ofSeconds(5);
+
+    private String hostId = "Blaze";
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() { // (2)
+        return registry -> registry.config()
+                .commonTags("host", hostId, "service", serviceId) // (3)
+                .meterFilter(MeterFilter.deny(id -> { // (4)
+                    String uri = id.getTag("uri");
+                    return uri != null && uri.startsWith("/swagger");
+                }))
+                .meterFilter(new MeterFilter() {
+                    @Override
+                    public DistributionStatisticConfig configure(Meter.Id id,
+                                                                 DistributionStatisticConfig config) {
+                        return config.merge(DistributionStatisticConfig.builder()
+                                .percentilesHistogram(true)
+                                .percentiles(0.5, 0.75, 0.95) // (5)
+                                .expiry(HISTOGRAM_EXPIRY) // (6)
+                                .bufferLength((int) (HISTOGRAM_EXPIRY.toMillis() / STEP.toMillis())) // (7)
+                                .build());
+                    }
+                });
+    }
+
+}
