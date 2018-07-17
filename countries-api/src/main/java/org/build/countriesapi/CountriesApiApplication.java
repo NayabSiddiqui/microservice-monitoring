20fdaae3ed3ff2de49ec906193d727da5094f96a
Nayab Siddiqui <nayab.s86@gmail.com>
2018-07-17 21:10:31 +0530

    skipped files

---
+++ countries-api/src/main/java/org/build/countriesapi/CountriesApiApplication.java
@@ -0,0 +1,12 @@
+package org.build.countriesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesApiApplication.class, args);
+	}
+}
---
+++ countries-api/src/test/java/org/build/countriesapi/CountriesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.build.countriesapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CountriesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
